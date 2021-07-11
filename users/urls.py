c55aca4c3f09b5923f2576ac66f0ab0e6f915a30
lukoluke <83480437+lukoluke@users.noreply.github.com>
2021-07-10 22:42:09 -0400

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='index' ), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='index'), name='logout'),
 ]
